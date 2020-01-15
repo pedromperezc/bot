3b336f6eca4860b1ea36c3b91d278a4993128e9e
Pedro Perez <PPEREZ@gire.com>
2020-01-15 14:56:55 -0300

    instalacion body-parser

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 
 app.get('/', (req, res) => {
    res.send({'hi': 'there'})
 });
 
+app.use(bodyParser.json());
+require('./routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
